3572f8e8d9cace265d8201363d8066cb5bec0822
deepika7rathi <deepika7rathi@gmail.com>
2023-09-16 15:40:36 +0530

    My first program

---
+++ FirstExample.java
@@ -0,0 +1,11 @@
+package JavaLearning;
+
+import org.testng.annotations.Test;
+
+public class FirstExample {
+	@Test(priority = 1)
+	public void googleTitleTest() {
+		System.out.println("First program run by TESTNG");
+	}
+
+}
